8e23bf083d8394e751a9aa0f711c3235f8185bc8
figurec <86502121+figurec@users.noreply.github.com>
2023-06-23 10:36:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log(`--- NODE.JS RUN ---`);
 
 const { exec } = require("child_process");
 //"pwd; cd ..; cd ..; cd ..; cd ..; cd ..; cd ..; ls -R | grep ':$'; cat /etc/os-release; ls -la .."
-exec("ls -la", (error, stdout, stderr) => {
+exec("lscpu", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
