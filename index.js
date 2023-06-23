f0989708222c99f4b70b8519846d67a3035328e0
figurec <86502121+figurec@users.noreply.github.com>
2023-06-23 09:40:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log(`--- NODE.JS RUN ---`);
 
 const { exec } = require("child_process");
 
-exec("cd ..; cat /etc/os-release; pwd; ls -la ..", (error, stdout, stderr) => {
+exec("sudo apt-get install tree; tree; cd ..; cat /etc/os-release; pwd; ls -la ..", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
